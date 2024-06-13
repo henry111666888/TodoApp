7021cf039e6863ef1fd691908b4cb5ca8a1f726e
henry168 <henry168@mail.hamastar.com.tw>
2024-06-13 17:09:27 +0800

    不知道為什麼重開vscode後還有要上傳的

--- test/TodoApp.Application.Tests/obj/Debug/net8.0/TodoApp.Application.Tests.AssemblyInfo.cs
+++ test/TodoApp.Application.Tests/obj/Debug/net8.0/TodoApp.Application.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.Application.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b01fb0e762daba23ba59bf87aeec9a322c07ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0abf7cd43a651e4c1687b131a981e3e5f1e7357c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.Application.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.Application.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
